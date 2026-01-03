181700c26e5dc9c9a2741d8ce76e776ea66418f2
himxa <himxa0x@gmail.com>
2026-01-03 21:58:09 +0100

    remove unsued dep

---
+++ src/eth/minmal.sol
@@ -0,0 +1,23 @@
+//SPDX-Lincense-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract MinimalAccount is IAccount,Ownable{
+
+constructor()Ownable(msg.sender){
+    
+}
+
+
+ function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData){
+
+    }
+}
